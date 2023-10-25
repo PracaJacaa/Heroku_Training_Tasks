beb6d641e76a1965c315e1118873c3df42c0a5b1
PracaJacaa <Gipsiak77@gmail.com>
2023-10-25 21:24:30 +0200

    Heroku Add Remove Elements task

--- Heroku_AddRemove_Elements.py
+++ Heroku_AddRemove_Elements.py
@@ -1,3 +1,6 @@
+def Length(elements):
+    return len(elements)
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -10,8 +13,24 @@ element = driver.find_element(By.XPATH, '//*[@id="content"]/div/button')
 for i in range(100):
     element.click()
 
-for z in range(100):
-    Del_element = driver.find_element(By.CLASS_NAME, 'added-manually')
-    Del_element.click()
+added_elements = driver.find_elements(By.CLASS_NAME, 'added-manually')
+
+if Length(added_elements) > 0:
+    print("The item has been added.")
+    print("Number of elements: "+ str(Length(added_elements)))
+else:
+    print("The item has not been added.")
+
+
+elements_to_remove = driver.find_elements(By.CLASS_NAME, 'added-manually')
+for element in elements_to_remove:
+    driver.execute_script("arguments[0].remove();", element)
+
+added_elements_afterRemv = driver.find_elements(By.CLASS_NAME, 'added-manually')
+
+if Length(added_elements_afterRemv) > 0:
+    print("We failed, Number of elements left: "+ str(Length(added_elements_afterRemv)))
+else:
+    print("WE DID IT")
 
 driver.quit()
